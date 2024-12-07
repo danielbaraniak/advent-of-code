a340ce62558b949332921c3abcf871518d235a62
Daniel Baraniak <dbaraniak2000@gmail.com>
2024-12-07 09:05:16 +0100

    measure time

--- kotlin/src/Day01.kt
+++ kotlin/src/Day01.kt
@@ -1,4 +1,6 @@
+import java.util.LinkedList
 import kotlin.math.abs
+import kotlin.system.measureNanoTime
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -15,10 +17,38 @@ fun main() {
         return listLeft.zip(listRight).sumOf { (l, r) -> abs(l - r) }
     }
 
+    fun part1_sorted(input: List<String>): Int {
+        val separator = Regex("\\s+")
+        val listLeft = mutableListOf<Int>()
+        val listRight = mutableListOf<Int>()
+
+        for (line in input) {
+            val (nl, nr) = line.split(separator, 2).map { it.toInt() }
+            listLeft.add(nl)
+            listRight.add(nr)
+        }
+
+        return listLeft.sorted().zip(listRight.sorted()).sumOf { (l, r) -> abs(l - r) }
+    }
+
+    fun part1_linked(input: List<String>): Int {
+        val separator = Regex("\\s+")
+        val listLeft = LinkedList<Int>()
+        val listRight = LinkedList<Int>()
+
+        for (line in input) {
+            val (nl, nr) = line.split(separator, 2).map { it.toInt() }
+            listLeft.binarySearch(nl).let { listLeft.add(if (it < 0) -it - 1 else it, nl) }
+            listRight.binarySearch(nr).let { listRight.add(if (it < 0) -it - 1 else it, nr) }
+        }
+
+        return listLeft.sorted().zip(listRight.sorted()).sumOf { (l, r) -> abs(l - r) }
+    }
+
     fun part2(input: List<String>): Int {
         val separator = Regex("\\s+")
         val listLeft = mutableListOf<Int>()
-        val listRight = mutableMapOf<Int,Int>().withDefault { 0 }
+        val listRight = mutableMapOf<Int, Int>().withDefault { 0 }
 
         for (line in input) {
             val (nl, nr) = line.split(separator, 2).map { it.toInt() }
@@ -30,6 +60,10 @@ fun main() {
     }
 
     val input = readInput("input_01")
-    part1(input).println()
-    part2(input).println()
+    //
+//        measureNanoTime { part1(input) }.println()
+        measureNanoTime { part1_sorted(input) }.println()
+//        measureNanoTime { part1_linked(input) }.println()
+//    part1(input).println()
+//    part2(input).println()
 }
