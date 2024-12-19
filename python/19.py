050cd830b0e4981c13c581e09cb5fd99853659be
Daniel Baraniak <dbaraniak2000@gmail.com>
2024-12-19 21:41:34 +0100

    day 19 part 2

--- python/19.py
+++ python/19.py
@@ -1,3 +1,6 @@
+from tqdm import tqdm
+
+
 def get_input(file_path: str) -> tuple[set[str], list[str]]:
     with open(file_path) as f:
         available, desired = f.read().strip().split("\n\n")
@@ -18,11 +21,40 @@ def is_possible(available: list[str], desired_towel: str) -> bool:
     return False
 
 
+def count_possible_combinations(available: list[str], desired_towel: str,  cache: dict[str, int]) -> bool:
+
+    if desired_towel in cache:
+        return cache[desired_towel]
+    counter = 0
+
+    if not desired_towel:
+        return 1
+
+    for i in range(1, len(desired_towel) + 1):
+        towel_part = desired_towel[:i]
+        if towel_part in available:
+            counter += count_possible_combinations(
+                available, desired_towel[i:], cache)
+
+    cache[desired_towel] = counter
+    return counter
+
+
 def count_possible_towels(available: list[str], desired: list[str]) -> int:
     possible = 0
     for desired_towel in desired:
         if is_possible(available, desired_towel):
             possible += 1
+    return possible
+
+
+def count_possible_combiations_total(available: list[str], desired: list[str]) -> int:
+    possible = 0
+    cache = {}
+    for desired_towel in tqdm(desired):
+        if is_possible(available, desired_towel):
+            possible += count_possible_combinations(
+                available, desired_towel, cache=cache)
     return possible
 
 
@@ -33,11 +65,26 @@ def test():
     assert possible == 6
 
 
+def test2():
+    available, desired = get_input("rust/data/examples/19.txt")
+    possible = count_possible_combiations_total(available, desired)
+    print(f"{available=}, {desired=}, {possible=}")
+    assert possible == 16
+
+
 def main():
     available, desired = get_input("rust/data/inputs/19.txt")
     possible = count_possible_towels(available, desired)
     print(possible)
 
 
+def main2():
+    available, desired = get_input("rust/data/inputs/19.txt")
+    possible = count_possible_combiations_total(available, desired)
+    print(possible)
+
+
 test()
+test2()
 main()
+main2()
